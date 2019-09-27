2d04ea930f5d7487a56a1d576d2f260eefac55e2
Sina Mahmoodi <itz.s1na@gmail.com>
2019-09-27 18:20:53 +0200

    ee: fix debug func names

--- assembly/debug.ts
+++ assembly/debug.ts
@@ -1,6 +1,10 @@
-export declare function debug(a: i32): void;
-export declare function debug_mem(pos: i32, len: i32): void;
+export declare function debug_print32(v: i32): void;
+export declare function debug_printMem(pos: i32, len: i32): void;
 
 export function debugMem(buf: Uint8Array): void {
-  debug_mem(buf.buffer as usize + buf.byteOffset, buf.length)
+  debug_printMem(buf.buffer as usize + buf.byteOffset, buf.length)
+}
+
+export function debug(v: i32): void {
+  debug_print32(v)
 }
