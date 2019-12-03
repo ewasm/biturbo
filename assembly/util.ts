a418cb926d918558c4c035ebd73d3a2e761634a3
Sina Mahmoodi <itz.s1na@gmail.com>
2019-12-03 11:36:25 +0100

    Add util u8ArrToUintArr

--- assembly/util.ts
+++ assembly/util.ts
@@ -132,3 +132,13 @@ export function nibbleArrToUintArr(arr: Array<u8>): Uint8Array {
 
   return buf;
 }
+
+export function u8ArrToUintArr(arr: Array<u8>): Uint8Array {
+  let buf = new Uint8Array(arr.length)
+
+  for (let i = 0; i < arr.length; i++) {
+    buf[i] = arr[i]
+  }
+
+  return buf
+}
