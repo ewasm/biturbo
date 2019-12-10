9966355b38a9ec7d261b89577b7bb0a5b180ac27
Sina Mahmoodi <1591639+s1na@users.noreply.github.com>
2019-12-10 15:55:29 +0100

    Setup separate build process for token & evm EE, run in CI (#32)
    * Setup separate build process for token & evm
    
    * ci: test both EEs
    
    * Update README.md with new build instructions
    
    * Fix token:run script
    
    * ci: disable evm ee for now

--- src/relayer/bin.ts
+++ src/relayer/bin.ts
@@ -26,22 +26,22 @@ async function main() {
 
     await testing.getTestsFromArgs('GeneralStateTests', async (filename: any, testName: any, test: any) => {
       const testSuite =  await stateTestRunner(runnerArgs, test, testName)
-      writeScoutConfig(testSuite, testCase + '.yaml')
+      writeScoutConfig(testSuite, testCase + '.yaml', 'build/evm_with_keccak.wasm')
     }, testGetterArgs).then( () => {
     }).catch((err: any) => {
       console.log('Err: ', err)
     })
   } else {
     const testSuite = await generateTestSuite()
-    writeScoutConfig(testSuite, 'turboproof.yaml')
+    writeScoutConfig(testSuite, 'turbo-token.yaml', 'build/token_with_keccak.wasm')
   }
-                          
+
 }
 
-function writeScoutConfig(data: TestSuite, filename: string) {
+function writeScoutConfig(data: TestSuite, outPath: string, wasmPath: string) {
   const testSuite = {
     beacon_state: {
-      execution_scripts: ['build/main_with_keccak.wasm'],
+      execution_scripts: [wasmPath],
     },
     shard_pre_state: {
       exec_env_states: [data.preStateRoot.toString('hex')],
@@ -53,7 +53,7 @@ function writeScoutConfig(data: TestSuite, filename: string) {
   }
 
   const serializedTestSuite = yaml.safeDump(testSuite)
-  fs.writeFileSync(filename, serializedTestSuite)
+  fs.writeFileSync(outPath, serializedTestSuite)
 }
 
 main()
