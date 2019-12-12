e3a06b01626f08ad3569659b4519106579d27211
Sina Mahmoodi <itz.s1na@gmail.com>
2019-12-12 14:09:45 +0100

    Add functions for evm code merkelization

--- src/relayer/lib.ts
+++ src/relayer/lib.ts
@@ -5,6 +5,7 @@ import { encode, decode } from 'rlp'
 import { Multiproof, verifyMultiproof, makeMultiproof, flatEncodeInstructions } from '../multiproof'
 import VM from 'ethereumjs-vm'
 import { Transaction } from 'ethereumjs-tx'
+import { getOpcodesForHF } from 'ethereumjs-vm/dist/evm/opcodes'
 
 const assert = require('assert')
 const { promisify } = require('util')
@@ -404,4 +405,66 @@ function rawMultiproof(proof: Multiproof, flatInstructions: boolean = false): an
       }),
     ]
   }
+}
+
+export function getBasicBlockIndices(code: Buffer): number[][] {
+  const TERMINATING_OPS = ['JUMP', 'JUMPI', 'STOP', 'RETURN', 'REVERT', 'SELFDESTRUCT']
+  const opcodes = getOpcodesForHF('istanbul')
+  const getOp = (i: number) => opcodes[code[i]] ? opcodes[code[i]].name : 'INVALID'
+
+  const blocks = [[0, -1]]
+  for (let i = 0; i < code.length; i++) {
+    let op = getOp(i)
+    // Skip push args
+    if (op === 'PUSH') {
+      i += code[i] - 0x5f
+    }
+
+    // Current instruction terminates block or next instruction is JUMPDEST
+    if (TERMINATING_OPS.includes(op) || (i + 1 < code.length && getOp(i + 1) === 'JUMPDEST')) {
+      blocks[blocks.length - 1][1] = i + 1
+      // Create new block if not at end of code
+      if (i + 1 < code.length) {
+        blocks.push([i + 1, -1])
+      }
+    }
+  }
+
+  // Close block if no terminating instruction at the end
+  if (blocks[blocks.length - 1][1] === undefined) {
+    blocks[blocks.length - 1][1] = code.length
+  }
+
+  return blocks
+}
+
+/**
+ * Does a single pass over bytecode to find the list
+ * of all basic blocks (i.e. blocks of code with no
+ * control flow change).
+ */
+export function getBasicBlocks(code: Buffer): Buffer[] {
+  const blocks = getBasicBlockIndices(code)
+  // Slice code based on block indices
+  return blocks.map((b: number[]) => code.slice(b[0], b[1]))
+}
+
+/**
+ * Divides code into basic blocks and constructs a MPT
+ * with these blocks as leaves. The key for each block is
+ * the index of the first byte of that block in the bytecode.
+ */
+export async function merkelizeCode(code: Buffer): Promise<any> {
+  const blockIndices = getBasicBlockIndices(code)
+  const trie = new Trie()
+  const putP = promisify(trie.put.bind(trie))
+  // Keys are indices into the bytecode. Determine key length by
+  // how large the last index is.
+  const keyLength = new BN(code.length - 1).byteLength()
+  for (let i = 0; i < blockIndices.length; i++) {
+    const key = new BN(blockIndices[i][0]).toBuffer('be', keyLength)
+    const val = code.slice(blockIndices[i][0], blockIndices[i][1])
+    await putP(key, val)
+  }
+  return trie
 }
